7eeeb30256a95d1101f582ec9206e52df4279999
Samu Encinas <samuel.encinasplaza@gmail.com>
2023-04-27 15:25:25 +0200

    Updates with comments

--- index.js
+++ index.js
@@ -3,15 +3,34 @@ import cors from 'cors';
 import  Movie  from './models/Movie.model.js';
 import  Cinema from './models/Cinema.model.js';
 import Knex from 'knex';
+import User from './models/User.model.js';
 import { development } from './knexfile.js';
 import ShowTiming  from './models/ShowTiming.model.js';
 import Timeslot  from './models/Timeslot.model.js';
+import session from 'express-session';
 import moment from 'moment';
+import passport from 'passport';
+import { strategyInit } from './lib/AuthStrategy.js';
 
 // Instanciamos Express y el middleware de JSON y CORS
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// Inicialización del sistema de sesiones (adelantando temario del siguiente lab)
+app.use(session({
+    secret: 'cines-session-cookie-key', // Secreto de la sesión (puede ser cualquier identificador unívoco de la app, esto no es público al usuario)
+    name: 'SessionCookie.SID', // Nombre de la sesión
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        maxAge: 3600000, // Expiración de la sesión
+    },
+}));
+app.use(passport.initialize()); // passport.initialize() inicializa Passport
+app.use(passport.session()); // passport.session() indica a Passport que usará sesiones
+strategyInit(passport);
 
 // Conexiones a la base de datos
 const dbConnection = Knex(development);
@@ -19,41 +38,54 @@ Movie.knex(dbConnection);
 Cinema.knex(dbConnection); 
 ShowTiming.knex(dbConnection);
 Timeslot.knex(dbConnection);
+User.knex(dbConnection);
 
-// Endpoint: POST /login --> Inicia sesión
-app.post("/login", (req, res, next) => {
-    passport.authenticate("local", (err, user, info) => {
-      if (err) throw err;
-      if (!user) res.send('No existe el usuario!');
-      else {
-        req.logIn(user, (err) => {
-          if (err) throw err;
-          res.send('Autenticación correcta');
-          console.log(req.user);
-        });
-      }
-    })(req, res, next);
-  });
+/**
+ * ENDPOINT: POST /login --> Inicia sesión
+ * 
+ * Si os dais cuenta, declaramos el endpoint metiendo passport.authenticate('local') como middleware 
+ * y después ya la lógica (que ocurrirá después del authenticate)
+ * 
+ * Para más info del authenticate ver el archivo LocalStrategy.js
+ */
+app.post('/login', passport.authenticate('local'), (req, res) => {
+    if (!!req.user) res.status(200).json({status: 'OK'})
+    else res.status(500).json({status: "Sesión no iniciada"});
+});
 
-// Endpoint: POST /register --> Registra un usuario
+
+
+/**
+ * ENDPOINT: POST /register --> Registra un usuario
+ * 
+ * Para registrar un usuario bastará con comprobar si existe y, si no existe, insertarlo en la base de datos.
+ * Para ello, podemos reciclar la query inicial añadiendo el método "insert"
+ * 
+ * El método findOne de objection es el equivalente a .where('columna', '=', valor).first(), pero simplificado
+ * Nota: Para poder llamar a findOne de este modo (recibiendo un JSON como parámetro), es preciso haber definido el JSONSchema en el modelo del Usuario
+ */
 app.post("/register", (req, res) => {
-    User.findOne({ username: req.body.username }, async (err, doc) => {
-      if (err) throw err;
-      if (doc) res.send('El usuario ya existe!');
-      if (!doc) {
-        const newUser = new User({
-          username: req.body.username,
-          password: req.body.password,
-        });
-        await newUser.save();
-        res.send('Usuario creado!');
-      }
-    });
+    const dbQuery = User.query();
+    dbQuery.findOne({user: req.body.user}).then(async result => {
+        if (!!result) res.status(500).json({error: "El usuario ya existe"});
+        else {
+            dbQuery.insert({
+                user: req.body.user,
+                unsecurePassword: String(req.body.password)
+            }).then(insertResult => {
+                debugger;
+            }).catch(error => {
+                debugger;
+            })
+        }
+
+    })
   });
 
-// Endpoint: GET /user --> Devuelve info del usuario
-app.get("/user", (req, res) => res.send(req.user));
+// Endpoint: GET /user --> Devuelve info del usuario en la sesión actual (y un 401 si no se ha iniciado sesión)
+app.get("/user", (req, res) => !!req.isAuthenticated() ? res.status(200).send(req.session) : res.status(401).send('Sesión no iniciada!'));
 
+// Endpoint: 
 // Endpoint: POST /movies --> Devuelve todas las películas
 app.post('/movies', (req, res) => {
     const consulta = Movie.query().throwIfNotFound();
@@ -150,8 +182,8 @@ app.post('/cinemas', (req, res) => {
 });
 
 // Definimos el puerto 3000 como puerto de escucha y un mensaje de confirmación cuando el servidor esté levantado
-app.listen(3000,() => {
-    console.log(`Servidor escuchando en el puerto 3000`);
+app.listen(8080,() => {
+    console.log(`Servidor escuchando en el puerto 8080`);
 });
 
 
--- models/User.model.js
+++ models/User.model.js
@@ -2,31 +2,26 @@ import {Model} from 'objection'
 import bcrypt from 'bcryptjs'
 
 export default class User extends Model {
-  static tableName = 'User';
+  static tableName = 'users';
+
+  static idColumn = 'id';
 
   static jsonSchema = {
     type: 'object',
-    required: ['username'],
+    required: ['user'],
 
     properties: {
-      id: {type: 'integer'},
-      name: {type: 'string'},
-      username: {type: 'string', default: ''},
-      hash: {type: 'string'},
-      isAdmin: {type: 'boolean', default: false},
-      isSeller: {type: 'boolean', default: false},
-      permissions: {type: 'array'},
-      created_at: {},
-      updated_at: {}
+      user: {type: 'string', default: ''},
+      password: {type: 'string'}
     }
   };
 
-  set password (password) {
-    this.hash = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+  set unsecurePassword (unsecurePassword) {
+    this.password = bcrypt.hashSync(unsecurePassword, bcrypt.genSaltSync(10))
   };
 
-  verifyPassword (password, callback) {
-    bcrypt.compare(password, this.hash, callback)
+  verifyPassword (unsecurePassword, callback) {
+    return bcrypt.compare(String(unsecurePassword), String(this.password), callback)
   };
 
 }
